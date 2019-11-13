aa481947d785884f918cc062ec3f48a5adb533d9
rsefraou <rita.sefraouitahiri@unil.ch>
2019-11-13 21:33:21 +0100

    Try taking off cycling dependencies

--- R/runDemo.R
+++ R/runDemo.R
@@ -1,7 +1,7 @@
 #' @title Run demonstration of app
 #'
 #' @describe returns  an estimated value of π and simulated points
-#' @import magrittr dplyr scales stringr kableExtra ggplot2 stats ptds2019hw4g1
+#' @import magrittr dplyr scales stringr  ggplot2 stats
 #' @export
 runDemo <- function() {
   appDir <- system.file("shiny-examples", "pi",  package = "ptds2019hw4g1")
