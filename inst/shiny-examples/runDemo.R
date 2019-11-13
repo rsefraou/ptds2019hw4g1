6f7aa63bd0c0d25da9646924dc196c628ae94f21
Barbusse <david.pitteloud@unil.ch>
2019-11-13 14:00:02 +0100

    Add the runDemo file

---
+++ inst/shiny-examples/runDemo.R
@@ -0,0 +1,14 @@
+#' @export
+runDemo <- function() {
+
+  appDir <- system.file("shiny-examples", "pi", package = "ptds2019hw4g1")
+  if (appDir == "") {
+    stop(
+      "Could not find example directory. Try re-installing ptds2019hw4g1.",
+      call. = FALSE
+    )
+  }
+
+  shiny::runApp(appDir, display.mode = "normal")
+
+}
