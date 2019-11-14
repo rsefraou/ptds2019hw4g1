6e8902993cf95fa0152ae1b1126d45d899618d13
rsefraou <rita.sefraouitahiri@unil.ch>
2019-11-14 19:40:37 +0100

    Remove comment

--- R/runDemo.R
+++ R/runDemo.R
@@ -7,7 +7,6 @@ runDemo <- function() {
   appDir <- system.file("shiny-examples", "pi",  package = "ptds2019hw4g1")
   if (appDir == "") {
     stop(
-      # REPLACE N BY YOUR GROUP NUMBER AND DELETE THIS COMMENT
       "Could not find example directory. Try re-installing ptds2019hw4g1.",
       call. = FALSE
     )
